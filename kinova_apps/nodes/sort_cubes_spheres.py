eabfbf054b0069e523cc88117599324d86e9ea4e
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2025-10-08 17:32:29 +0200

    remove msg filters and unused tf buffer

--- kinova_apps/nodes/sort_cubes_spheres.py
+++ kinova_apps/nodes/sort_cubes_spheres.py
@@ -3,28 +3,21 @@ import rclpy
 from rclpy.node import Node
 from rclpy.utilities import ok as rclpy_ok
 from rclpy.duration import Duration
-from rclpy.time import Time
+from rclpy.wait_for_message import wait_for_message
 
-from message_filters import ApproximateTimeSynchronizer, Subscriber
-
-from sensor_msgs.msg import PointCloud2, Image
+from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py import point_cloud2
 from std_msgs.msg import Header
-from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import Marker, MarkerArray
-
-from tf2_ros import TransformListener, Buffer
-import tf2_geometry_msgs
-from tf2_geometry_msgs import do_transform_pose_stamped
 
 import numpy as np
 import open3d as o3d
 from scipy.cluster.hierarchy import linkage, fcluster
 from sklearn.cluster import KMeans
 import cv2
-from cv_bridge import CvBridge
 from pydantic import BaseModel
 from enum import StrEnum
+from typing import Optional
 
 from kinova_apps.utils.pc_utils import cluster_pc, o3dpc_to_ros
 
@@ -62,29 +55,25 @@ class SortObjects(Node):
     def __init__(self):
         super().__init__('sort_objects')
 
-        self.tf2_buffer = Buffer()
-        self.tf2_listener = TransformListener(self.tf2_buffer, self)
-        
-        self.img_sub = Subscriber(self, Image, '/camera/color/image_raw')
-        self.pc_sub = Subscriber(self, PointCloud2, '/camera/depth/color/points')
-
         self.seg_plane_pub = self.create_publisher(PointCloud2, '/pc_plane', 10)
         self.pc_cluster_pub = self.create_publisher(PointCloud2, '/pc_cluster', 10)
         self.marker_pub = self.create_publisher(MarkerArray, '/detected_objects', 10)
 
-        self.ts = ApproximateTimeSynchronizer(
-            [self.img_sub, self.pc_sub], queue_size=10, slop=0.1)
-        self.ts.registerCallback(self.callback)
-
-        self.bridge = CvBridge()
-
         self.get_logger().info('SortObjects node started')
 
-    def callback(self, img_msg: Image, pc_msg: PointCloud2):
-        self.get_logger().info('Synchronized messages received')
-        
-        # rgb_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
-        
+    def detect(self):
+        ret = False
+        pc_msg: Optional[PointCloud2] = None
+        while not ret:
+            ret, pc_msg = wait_for_message(node=self, msg_type=PointCloud2, topic='/camera/depth/color/points')
+            if not rclpy_ok():
+                self.get_logger().info('Shutdown detected, exiting')
+                return
+            if not ret:
+                self.get_logger().info('Waiting for point cloud message...')
+
+        assert isinstance(pc_msg, PointCloud2)
+
         pc = point_cloud2.read_points_numpy(pc_msg, skip_nans=False, reshape_organized_cloud=True)
         # replace nan with 0
         pc = np.nan_to_num(pc, nan=0.0)
@@ -105,17 +94,11 @@ class SortObjects(Node):
         self.pc_cluster_pub.publish(o3dpc_to_ros(obj_cluster_cloud, pc_header.frame_id, pc_header.stamp))
         self.publish_markers(clusters, pc_header)
 
-        # stop subscriptions
-        self.destroy_subscription(self.img_sub.sub)
-        self.destroy_subscription(self.pc_sub.sub)
-
-
     def detect_pc_objects(self, pc: np.ndarray) -> tuple[o3d.geometry.PointCloud, o3d.geometry.PointCloud, list[ClusterInfo]]:
         plane_cloud, obj_cluster_cloud, labels = cluster_pc(pc)
         clusters = process_clusters_cube_sphere(obj_cluster_cloud, labels)
 
         return plane_cloud, obj_cluster_cloud, clusters
-
 
     def publish_markers(self, clusters: list[ClusterInfo], pc_header: Header):
         marker_array = MarkerArray()
@@ -160,6 +143,7 @@ class SortObjects(Node):
             marker_id += 1
 
         self.marker_pub.publish(marker_array)
+
 
 def process_clusters_cube_sphere(
     object_cloud: o3d.geometry.PointCloud,
@@ -281,6 +265,7 @@ def get_color_label(rgb: list[float]) -> ColorLabel:
 def main():
     rclpy.init()
     node = SortObjects()
+    node.detect()
     
     while rclpy_ok():
         rclpy.spin_once(node)
